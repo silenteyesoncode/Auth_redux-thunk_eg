120ccc2c429cd7d6bee87119e6eba8a965644e83
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-10-06 11:49:47 +0530

    Initial commit

---
+++ src/store/actions/index.js
@@ -0,0 +1,3 @@
+export { addPlace, deletePlace, getPlaces } from "./places";
+export { tryAuth, authGetToken, authAutoSignIn, authLogout } from "./auth";
+export { uiStartLoading, uiStopLoading } from "./ui";
